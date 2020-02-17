b323dd4042ab64d696371451349ae12984fdefc3
lizhenxiang <786321342@qq.com>
2020-02-17 21:51:42 +0800

    dev分支，添加一个Second类

---
+++ day01_sparsearray/src/com/atguigu/day02_linkedlist/TestStack.java
@@ -0,0 +1,14 @@
+package com.atguigu.day02_linkedlist;
+
+import java.util.Stack;
+
+public class TestStack {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack();
+
+        //入栈
+        stack.add("jack");
+        stack.add("tom");
+        stack.add("smith");
+    }
+}
